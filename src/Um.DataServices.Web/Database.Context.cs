fcaf6d583d170c50bddf21afc85de48a9858c6b7
mas <mas@globeteam.com>
2015-01-29 23:54:39 +0100

    Updated with new Activities view

--- src/Um.DataServices.Web/Database.Context.cs
+++ src/Um.DataServices.Web/Database.Context.cs
@@ -35,6 +35,7 @@ namespace Um.DataServices.Web
         public virtual DbSet<Sector> Sectors { get; set; }
         public virtual DbSet<Channel> Channels { get; set; }
         public virtual DbSet<Region> Regions { get; set; }
+        public virtual DbSet<Activity> Activities { get; set; }
     
         public virtual ObjectResult<Activities_Result> GetActivitiesXml(string recipientCountry, string region, string sector)
         {
